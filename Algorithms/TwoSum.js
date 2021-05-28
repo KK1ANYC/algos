242d93c6a70eaa7c92d1f5f59e335433a97eac36
Kevin Kim <kevin.kim9411@gmail.com>
2021-05-28 13:07:05 -0400

    twoSum solution 1

--- Algorithms/TwoSum.js
+++ Algorithms/TwoSum.js
@@ -1,4 +1,23 @@
 /*
+Two Number Sum
+
 Find any two numbers in the array that sums up to the target sum
 */
 
+//solution 1
+function twoSum(array, target) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right++;
+    }
+  }
+  return []
+}
