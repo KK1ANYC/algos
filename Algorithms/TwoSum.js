6486510b3e13fa53c8f1a1be5bacff14ba855de1
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-07 10:26:12 -0400

    twoSum: double for loop

--- Algorithms/TwoSum.js
+++ Algorithms/TwoSum.js
@@ -23,4 +23,16 @@ function twoSum(array, target) {
 }
 
 //solution 2
-
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+	for (let i = 0; i < array.length - 1; i++) {
+		const firstNum = array[i]
+		for (let j = i + 1; j < array.length; j++) {
+			const secondNum = array[j]
+			if (firstNum + secondNum === targetSum) {
+				return [firstNum, secondNum]
+			}
+		}
+	}
+	return []
+}
