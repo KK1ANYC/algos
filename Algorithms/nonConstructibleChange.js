464c51833eb10369092cf298b0959bfe1e8738c5
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-08 11:03:21 -0400

    non constructible change

---
+++ Algorithms/nonConstructibleChange.js
@@ -0,0 +1,15 @@
+//non Constructible change
+
+//Solution 1
+function nonConstructibleChange(coins) {
+  // Write your code here.
+	coins.sort((a,b) => a-b)
+	let sum = 0;
+	for (let i = 0; i < coins.length; i++) {
+		if ( coins[i] > sum + 1) {
+			return sum + 1
+		}
+		sum += coins[i]
+	}
+	return sum + 1
+}
