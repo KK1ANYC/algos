5b742f7b048f93b22067975ddd880834230ecd83
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-09 11:46:43 -0400

    isPalindrome solution

--- Algorithms/isPalindrome.js
+++ Algorithms/isPalindrome.js
@@ -1,4 +1,21 @@
 //isPalindrome
 
-//solution 1
-
+//solution 1 O(n) time | O(n) space
+function isPalindrome(string) {
+  // Write your code here.
+	if (string.length === 1) {
+		return true
+	}
+	if (string.length === 2 && string[0] === string[string.length - 1]) {
+		return true
+	}
+	if (string.length > 2) {
+		if (string[0] === string[string.length - 1]) {
+			let sliceStr = string.slice(1, string.length - 1)
+			return isPalindrome(sliceStr)
+		} else {
+			return false
+		}
+	}
+	return false
+}
