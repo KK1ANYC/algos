138d230d71c0989b19fc750abd9f87cc2925dd0e
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-10 12:19:32 -0400

    firstNonRepeatinCharacter solution 1

--- Algorithms/firstNonRepeatingCharacter.js
+++ Algorithms/firstNonRepeatingCharacter.js
@@ -1,3 +1,27 @@
 //firstNonRepeatingCharacter
 
 //solution 1
+//O(n + m) time | O(m) space
+function firstNonRepeatingCharacter(string) {
+  // Write your code here.
+	let memo = {};
+	let char = false
+	for (let i of string) {
+		!memo[i] ? memo[i] = 1 : memo[i]++
+	}
+	for (let key in memo) {
+		if (memo[key] === 1) {
+			char = key
+			break
+		}
+	}
+	if (char) {
+		for (let i = 0; i < string.length; i++) {
+			if (string[i] === char) {
+				return i
+			}
+		}
+	} else {
+		return -1
+	}
+}
