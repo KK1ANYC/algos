27f274768d9200e5fbaea654c10ffe099043654d
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-10 11:44:13 -0400

    generateDocument solution 1

--- Algorithms/generateDocument.js
+++ Algorithms/generateDocument.js
@@ -1,3 +1,23 @@
 //generateDocument
 
 //solution 1
+//O(m * (n + m)) time | O(n) space - where n is the number of characters and m is the length of the document
+function generateDocument(characters, document) {
+  // Write your code here.
+	let memo = {};
+	for (let i = 0; i < characters.length; i++) {
+		if (memo[characters[i]]) {
+			memo[characters[i]]++
+		} else {
+			memo[characters[i]] = 1
+		}
+	}
+	for (let i = 0; i < document.length; i++) {
+		if (memo[document[i]] && memo[document[i]] >= 1) {
+			memo[document[i]]--
+		} else {
+			return false
+		}
+	}
+  return true;
+}
