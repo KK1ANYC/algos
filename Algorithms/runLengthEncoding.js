89bb371d1b10f038692aab3285006fda6bf3be0f
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-10 11:12:36 -0400

    runLengthEncoding solution 1

--- Algorithms/runLengthEncoding.js
+++ Algorithms/runLengthEncoding.js
@@ -1,3 +1,26 @@
 //runLengthEncoding
 
 //solution 1
+//O(n) time | O(n) space - where n is the length of the input string
+function runLengthEncoding(string) {
+  // Write your code here.
+	const encodedStringChar = [];
+	let currRunLength = 1;
+
+	for (let i = 1; i < string.length; i++) {
+		const currChar = string[i]
+		const prevChar = string[i - 1]
+
+		if (currChar !== prevChar || currRunLength === 9) {
+			encodedStringChar.push(currRunLength.toString())
+			encodedStringChar.push(prevChar)
+			currRunLength = 0
+		}
+		currRunLength++
+	}
+	encodedStringChar.push(currRunLength.toString())
+	encodedStringChar.push(string[string.length - 1])
+
+	return encodedStringChar.join('')
+}
+
