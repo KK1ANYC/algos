1d207eee73f547fe3893f106325c26e31ab8e78f
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-11 22:27:31 -0400

    recursion palindrome

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -13,7 +13,15 @@ console.log(reverse('rithmschool')) // 'loohcsmhtir'
 
 //palindrome
 function palindrome(string) {
-
+ if (string.length === 1) return true
+ if (string.length === 2) return string[0] === string[1]
+ let firstEl = string[0]
+ let lastEl = string[string.length - 1]
+ if (firstEl !== lastEl) {
+	 return false
+ }
+ let isPalindrome = palindrome(string.slice(1, -1))
+ return isPalindrome
 }
 
 console.log(palindrome("tacocat")) // true
