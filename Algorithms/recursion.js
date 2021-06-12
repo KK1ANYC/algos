28f61d60e433c906c9cef1239bd0a0e42b165229
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-11 23:24:49 -0400

    recursion flatten example 2

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -40,19 +40,35 @@ console.log(someRecursive([4,6,8], isOdd)) // false
 console.log(someRecursive([4,6,8], val => val > 10)) // false
 
 //Flatten Array
-function flatten(nestedArr){
-	let arr = []
-	for (let i = 0; i < nestedArr.length; i++) {
-	  let el = nestedArr[i]
-	  if (Array.isArray(el)) {
-	    arr = arr.concat(flatten(el))
-	  } else {
-	  arr.push(el)
-	  }
-	}
-	return arr
-      }
 
-      console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
-      console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-      console.log(flatten([[1],[2],[3]])) // [1,2,3]
+//example 1
+function flatten(nestedArr){
+let arr = []
+for (let i = 0; i < nestedArr.length; i++) {
+let el = nestedArr[i]
+if (Array.isArray(el)) {
+arr = arr.concat(flatten(el))
+} else {
+arr.push(el)
+}
+}
+return arr
+}
+
+//example 2
+function flatten(array){
+let arr = []
+if (array.length == 0) return arr;
+if (Array.isArray(array[0])) {
+arr = arr.concat(flatten(array[0]));
+} else {
+arr.push(array[0]);
+}
+arr = arr.concat(flatten(array.slice(1)))
+
+return arr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
