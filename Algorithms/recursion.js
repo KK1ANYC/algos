aedc122bc11983126789de96b7253504e639d61d
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-11 22:05:31 -0400

    recursion reverse String

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -1,3 +1,13 @@
 //recursion
 
 //reverse String
+function reverse(string){
+if (string.length === 0) return ""
+let lastEl = string[string.length - 1]
+let reverseString = reverse(string.slice(0, -1))
+return lastEl += reverseString
+}
+
+console.log(reverse('awesome')) // 'emosewa'
+console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
