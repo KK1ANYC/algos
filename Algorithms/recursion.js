1f302ed40b4a91e98fb8aeff72ccc5797ef6459d
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-11 22:52:41 -0400

    recursion isTrue

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -25,3 +25,16 @@ function palindrome(string) {
 }
 
 console.log(palindrome("tacocat")) // true
+
+//isTrue
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(nums, callback){
+  if (nums.length === 1 && !callback(nums[nums.length-1])) return false
+  if (callback(nums[nums.length-1])) return true
+  return someRecursive(nums.slice(0,-1), callback)
+}
+
+console.log(someRecursive([4,6,8,9], isOdd)) // true
+console.log(someRecursive([4,6,8], isOdd)) // false
+console.log(someRecursive([4,6,8], val => val > 10)) // false
