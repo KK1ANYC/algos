68e7cf2c298f7f93e5aaf50d7611143c49d08b33
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 11:59:02 -0400

    collectStrings

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -167,3 +167,37 @@ function stringifyNumbers(obj) {
 }
 
 console.log(stringifyNumbers(obj));
+
+//example 7
+function collectStrings(obj) {
+  let result = [];
+  function recursiveHelp(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === "object") {
+        recursiveHelp(obj[key]);
+      } else if (typeof obj[key] === "string") {
+        result.push(obj[key]);
+      }
+    }
+  }
+  recursiveHelp(obj);
+  return result;
+}
+
+const obj1 = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+console.log(collectStrings(obj1)); // ["foo", "bar", "baz"])
