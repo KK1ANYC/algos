0d944e5c8b28e6cf33f9b485010b7ff5368ccead
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 11:10:17 -0400

    recursion nestedEvenSum

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -91,3 +91,30 @@ function capitalizeFirst (arr) {
 
 
       console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
+
+//example 4
+
+var obj1 = {
+	a: 2,
+	b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+	c: {c: {c: 2}, cc: 'ball', ccc: 5},
+	d: 1,
+	e: {e: {e: 2}, ee: 'car'}
+      };
+
+      function nestedEvenSum (obj) {
+	let sum = 0
+	function recursiveHelp(obj) {
+	  for (let key in obj) {
+	    if (typeof obj[key] === 'object') {
+	      recursiveHelp(obj[key])
+	    } else if (obj[key] % 2 === 0) {
+	      sum += obj[key]
+	    }
+	  }
+	}
+	recursiveHelp(obj)
+	return sum
+      }
+
+      console.log(nestedEvenSum(obj1)); // 10
