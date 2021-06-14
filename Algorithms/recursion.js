501548742640c6d2c89e7443b66ddc26e7716e8a
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 11:29:13 -0400

    recursion capitalize Words

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -118,3 +118,21 @@ var obj1 = {
       }
 
       console.log(nestedEvenSum(obj1)); // 10
+
+//example 5
+function capitalizeWords (words) {
+let result = []
+function recursiveHelp(arr) {
+	if (arr.length === 0) return
+	if (arr.length >= 1) {
+	result.push(arr[0].toUpperCase())
+	}
+	return recursiveHelp(arr.slice(1))
+}
+recursiveHelp(words)
+return result
+}
+
+let words = ['i', 'am', 'learning', 'recursion']
+
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
