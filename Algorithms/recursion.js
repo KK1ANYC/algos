3613fa56c10cffa7b9ee561170285695fada280b
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 10:41:46 -0400

    recursion capitalizeFirst

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -72,3 +72,22 @@ return arr;
 console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
 console.log(flatten([[1],[2],[3]])) // [1,2,3]
+
+//example 3
+function capitalizeFirst (arr) {
+	let result = []
+	function recursiveHelp(arr) {
+	  if (arr.length === 0) {
+	  return;
+	  }
+	  if (arr.length >= 1) {
+	    result.push(arr[0][0].toUpperCase() + arr[0].slice(1))
+	  }
+	  return recursiveHelp(arr.slice(1))
+	}
+	recursiveHelp(arr)
+	return result
+      }
+
+
+      console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana']
