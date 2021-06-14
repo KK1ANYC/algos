9c65ab9b506df14f693b66f68db74148dd25442f
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 16:02:24 -0400

    searchAlgo, KMP

--- Algorithms/searchAlgo.js
+++ Algorithms/searchAlgo.js
@@ -52,13 +52,33 @@ function stringSearch(string, target) {
 
 // naive search 2
 function stringSearch(string, target) {
-	let totalCount = 0;
-	for (let i = 0; i < string.length; i++) {
-	  for (let j = 0; j < target.length; j++) {
-	    let cj = target[j];
-	    if (cj !== string[i + j]) break;
-	    if (j === target.length - 1) totalCount++;
-	  }
-	}
-	return totalCount;
+  let totalCount = 0;
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < target.length; j++) {
+      let cj = target[j];
+      if (cj !== string[i + j]) break;
+      if (j === target.length - 1) totalCount++;
+    }
+  }
+  return totalCount;
+}
+
+//KMP
+function stringSearch(string, target) {
+  let totalCount = 0;
+  let place = 0;
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+    for (let j = 0; j < target.length; j++) {
+      let cj = target[j];
+      place = 0;
+      if (cj !== string[i + j]) break;
+      place = j + 1;
+      if (j === target.length - 1) {
+        totalCount++;
       }
+    }
+    i = i + place;
+  }
+  return totalCount;
+}
