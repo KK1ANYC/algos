2a25546d1d083e1c42e3349f8c472f1e272cd18e
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 17:08:22 -0400

    sortingAlgo bubble sort

---
+++ Algorithms/sortingAlgo.js
@@ -0,0 +1,15 @@
+//bubble sort
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([15, 10, 3, 1, 4, 7, 5]));
