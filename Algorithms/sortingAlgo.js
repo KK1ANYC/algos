772198984b16f833e65d4ab5abf23e1b83ccdf63
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-14 23:36:51 -0400

    sortingAlgo, insertionSort

--- Algorithms/sortingAlgo.js
+++ Algorithms/sortingAlgo.js
@@ -89,3 +89,15 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([15, 10, 3, 1, 4, 7, 5]));
+
+//Insertion Sort
+function insertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
