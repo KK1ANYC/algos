7e72db6944da9c98eaaa0a657623a20e5bcb4591
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-15 19:38:06 -0400

    sorting Algo radix sort solution

--- Algorithms/sortingAlgo.js
+++ Algorithms/sortingAlgo.js
@@ -229,3 +229,44 @@ function swap(arr, i, j) {
 }
 
 //radix sort
+function getDigit(num, i) {
+  return Math.floor((Math.abs(num) / Math.pow(10, i)) % 10);
+}
+
+function digitCount(num, i) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num)) + 1);
+}
+
+function mostDigits(num) {
+  let maxDigits = 0;
+  for (let i = 0; i < num.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(num[i]));
+  }
+  return maxDigits;
+}
+
+const radixSort = (arr) => {
+  let maxD = 0;
+  maxD = mostDigits(arr);
+  let radixArr = [[], [], [], [], [], [], [], [], [], []];
+  for (let i = 0; i < maxD; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let curr = arr[j];
+      if (getDigit(curr, i)) radixArr[getDigit(curr, i)].push(curr);
+      else {
+        radixArr[0].push(curr);
+      }
+    }
+    arr = [];
+    for (let k = 0; k < radixArr.length; k++) {
+      let indexArr = radixArr[k];
+      while (indexArr.length) {
+        arr.push(indexArr.shift());
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(radixSort([56, 345, 43, 6, 8, 200])); // 4
