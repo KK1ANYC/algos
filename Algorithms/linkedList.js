2488f8f43e9582a0a48dc05bf84282d3ea55e38f
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-17 11:23:06 -0400

    linkedList doubly linked list set up

--- Algorithms/linkedList.js
+++ Algorithms/linkedList.js
@@ -141,3 +141,22 @@ console.log(list);
 
 //doubly linked list
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class doublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+let list = new doublyLinkedList();
+
+console.log(list);
