1df7e69d6262bf3f123ee2f2dc5b3f5c9bc4ead7
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-17 11:35:47 -0400

    linkedList doubly linked list push

--- Algorithms/linkedList.js
+++ Algorithms/linkedList.js
@@ -155,8 +155,23 @@ class doublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new doublyLinkedList();
 
+list.push(1);
 console.log(list);
