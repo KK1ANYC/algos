b049bedb8db603d3068ab347ebac99513a089fd0
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-18 17:27:58 -0400

    queue class

---
+++ Algorithms/queue.js
@@ -0,0 +1,20 @@
+//Queue
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.dequeue();
+console.log(queue);
