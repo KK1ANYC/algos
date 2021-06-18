544c36babf2c1f95756c4b124f9928fb64e4b574
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-18 16:53:23 -0400

    stacks example 1

--- Algorithms/stacks.js
+++ Algorithms/stacks.js
@@ -1,1 +1,42 @@
 //stacks
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      // let temp = this.first
+      // this.first = newNode
+      // this.first.next = temp
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    return ++this.size;
+  }
+  pop() {
+    let newFirst = this.first;
+    if (!this.first) return null;
+    else if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      newFirst = this.first.next;
+      this.first = null;
+      this.first = newFirst;
+    }
+    this.size--;
+    return newFirst.val;
+  }
+}
