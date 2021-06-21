751492f0aad61acb893eda9b8fc72522296108cb
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-21 13:55:00 -0400

    bst insert update 3

--- Algorithms/binarySearchTree.js
+++ Algorithms/binarySearchTree.js
@@ -11,6 +11,24 @@ class BinarySearchTree {
   constructor() {
     this.root;
   }
+//   insert(val) {
+//     let newNode = new Node(val);
+//     let current = this.root;
+//     if (!this.root) this.root = newNode;
+//     else {
+//       while (current) {
+//         if (val === current.val) return undefined;
+//         if (val > current.val && current.right) {
+//           current = current.right;
+//         } else if (val < current.val && current.left) {
+//           current = current.left;
+//         } else break;
+//       }
+//       val > current.val ? (current.right = newNode) : (current.left = newNode);
+//     }
+//     return this;
+//   }
+
   insert(val) {
     let newNode = new Node(val);
     let current = this.root;
