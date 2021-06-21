992c7a3a0e24f91dbf29ba8c48d991ac9818f447
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-21 16:56:28 -0400

    bst bfs

--- Algorithms/binarySearchTree.js
+++ Algorithms/binarySearchTree.js
@@ -72,4 +72,19 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  BFS() {
+    //iterative solution
+    let queue = [];
+    let visited = [];
+    let current = this.root;
+    queue.push(current);
+    while (queue.length) {
+      current = queue.shift();
+      visited.push(current.val);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return visited;
+  }
 }
