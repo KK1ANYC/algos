65f8f74936cf4b423b5d8a08317530ed86c3c367
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-22 16:35:03 -0400

    binary heaps extract max

--- Algorithms/binaryHeaps.js
+++ Algorithms/binaryHeaps.js
@@ -18,6 +18,35 @@ class MaxBinaryHeap {
     }
     return this.values;
   }
+
+  extractMax() {
+    let temp = this.values[0];
+    this.values[0] = this.values[this.values.length - 1];
+    this.values[this.values.length - 1] = temp;
+    let removedRoot = this.values.pop();
+    let i = 0;
+    while (i < this.values.length) {
+      let left = i * 2 + 1;
+      let right = i * 2 + 2;
+      if (
+        this.values[i] > this.values[left] &&
+        this.values[i] > this.values[right]
+      )
+        break;
+      else if (this.values[left] > this.values[right]) {
+        let temp = this.values[left];
+        this.values[left] = this.values[i];
+        this.values[i] = temp;
+        i = left;
+      } else if (this.values[right] > this.values[left]) {
+        let temp = this.values[right];
+        this.values[right] = this.values[i];
+        this.values[i] = temp;
+        i = right;
+      } else break;
+    }
+    return removedRoot;
+  }
 }
 let heap = MaxBinaryHeap();
 console.log(heap);
