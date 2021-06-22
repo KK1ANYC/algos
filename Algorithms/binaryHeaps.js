dfa89cda7b0a6caef7f33ee2c8834fd97e9d7b09
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-22 15:05:19 -0400

    binary heap insert

--- Algorithms/binaryHeaps.js
+++ Algorithms/binaryHeaps.js
@@ -3,7 +3,21 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
-  insert() {}
+  insert(val) {
+    let i = this.values.length;
+    let j = Math.floor((i - 1) / 2);
+    this.values.push(val);
+    while (i >= 0) {
+      if (this.values[j] < val) {
+        let temp = this.values[j];
+        this.values[j] = this.values[i];
+        this.values[i] = temp;
+      }
+      i = j;
+      j = Math.floor((i - 1) / 2);
+    }
+    return this.values;
+  }
 }
 let heap = MaxBinaryHeap();
 console.log(heap);
