f696f8554e88b8898b5ae2d7865bc9be92e6df1d
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-24 15:04:46 -0400

    graph removeEdge

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -12,6 +12,23 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) {
+      for (let i = 0; i < this.adjacencyList[vertex1].length; i++) {
+        if (this.adjacencyList[vertex1][i] === vertex2)
+          this.adjacencyList[vertex1].splice(i, 1);
+      }
+    }
+    if (this.adjacencyList[vertex2]) {
+      for (let i = 0; i < this.adjacencyList[vertex2].length; i++) {
+        if (this.adjacencyList[vertex2][i] === vertex1)
+          this.adjacencyList[vertex2].splice(i, 1);
+      }
+    } else {
+      return undefined;
+    }
+  }
 }
 
 let graph = new Graph();
