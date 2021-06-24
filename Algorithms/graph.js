00777c3d833cdc5d70cfcff08f9ebcf80c9ef55c
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-24 12:33:06 -0400

    graph addEdge

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -7,6 +7,11 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 let graph = new Graph();
