aea28b461a7836a25146e54492982068894cc587
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-28 19:24:06 -0400

    graph dijkstra reorganize

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -134,38 +134,6 @@ g.depthFirstRecursive("A");
 console.log(g);
 
 //Dijkstra's
-class WeightedGraph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-  }
-  addEdge(vertex1, vertex2, weight) {
-    this.adjacencyList[vertex1].push({ node: vertex2, weight });
-    this.adjacencyList[vertex2].push({ node: vertex1, weight });
-  }
-}
-
-let g = new WeightedGraph();
-
-g.addVertex("A")
-g.addVertex("B")
-g.addVertex("C")
-g.addVertex("D")
-g.addVertex("E")
-g.addVertex("F")
-
-g.addEdge("A", "B", 4)
-g.addEdge("A", "C", 2)
-g.addEdge("B","E", 3)
-g.addEdge("C","D", 2)
-g.addEdge("C","F", 4)
-g.addEdge("D","E", 3)
-g.addEdge("D","F", 1)
-g.addEdge("E","F", 1)
-
-g.dijkstra("A", "E")
 
 class PriorityQueue {
   constructor() {
@@ -182,3 +150,36 @@ class PriorityQueue {
     this.values.sort((a, b) => a.priority - b.priority);
   }
 }
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
+
+let g = new WeightedGraph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B", 4);
+g.addEdge("A", "C", 2);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "E", 3);
+g.addEdge("D", "F", 1);
+g.addEdge("E", "F", 1);
+
+g.dijkstra("A", "E");
