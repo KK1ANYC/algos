9e67f1c3cf31fbb60d18dad38c7b191663ed7956
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-28 18:09:46 -0400

    Dijkstra addVertex and addEdge

--- Algorithms/graph.js
+++ Algorithms/graph.js
@@ -134,3 +134,15 @@ g.depthFirstRecursive("A");
 console.log(g);
 
 //Dijkstra's
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
