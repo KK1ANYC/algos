3715dbb2f232e8dfc3fa14ee76e3131840ff0704
Kevin Kim <kevin.kim9411@gmail.com>
2021-06-30 17:26:21 -0400

    recursion product sum

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -201,3 +201,21 @@ const obj1 = {
 };
 
 console.log(collectStrings(obj1)); // ["foo", "bar", "baz"])
+
+//product sum
+
+//O(n) time | O(d) space - where n is the total number of elements in the array
+//including sub-elements, and d is the greatest depth of "special" arrays in the array
+
+function productSum(array, multiplier = 1) {
+  // Write your code here.
+	let sum = 0
+	for (const element of array) {
+		if (Array.isArray(element)) {
+			sum += productSum(element, multiplier + 1)
+		} else {
+			sum += element
+		}
+	}
+	return sum * multiplier
+}
