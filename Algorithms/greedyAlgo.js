8c4217113e64f53119cb75e602cf0b92d42e90bb
Kevin Kim <kevin.kim9411@gmail.com>
2021-07-03 16:36:37 -0400

    greedy algo class photo

--- Algorithms/greedyAlgo.js
+++ Algorithms/greedyAlgo.js
@@ -10,3 +10,26 @@ function minimumWaitingTime(arr) {
   }
   return sum;
 }
+
+//class photo
+//O(nlog(n)) time | O(1) space - where n is the number of students
+function classPhotos(redShirtHeights, blueShirtHeights) {
+  // Write your code here.
+  redShirtHeights.sort((a, b) => b - a);
+  blueShirtHeights.sort((a, b) => b - a);
+  let backRow = [];
+  let frontRow = [];
+  if (blueShirtHeights[0] > redShirtHeights[0]) {
+    backRow = blueShirtHeights;
+    frontRow = redShirtHeights;
+  } else {
+    backRow = redShirtHeights;
+    frontRow = blueShirtHeights;
+  }
+  for (let i = 0; i < backRow.length; i++) {
+    if (backRow[i] <= frontRow[i]) {
+      return false;
+    }
+  }
+  return true;
+}
