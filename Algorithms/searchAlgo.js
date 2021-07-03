ba5a37c9c7d0da781b917dd7e96aa4226a085963
Kevin Kim <kevin.kim9411@gmail.com>
2021-07-03 12:02:53 -0400

    search Algo three largest num example 2

--- Algorithms/searchAlgo.js
+++ Algorithms/searchAlgo.js
@@ -84,6 +84,7 @@ function stringSearch(string, target) {
 }
 
 //Three largest number
+//example 1
 function findThreeLargestNumbers(arr) {
   let result = [null, null, null];
   for (let i = 0; i < arr.length; i++) {
@@ -102,4 +103,36 @@ function findThreeLargestNumbers(arr) {
     }
   }
   return result;
+}
+
+//example 2
+//O(n) time | O(1) space
+
+function findThreeLargestNumbers(array) {
+  // Write your code here.
+  const threeLargest = [null, null, null];
+  for (const num of array) {
+    updateLargest(threeLargest, num);
+  }
+  return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+  if (threeLargest[2] === null || num > threeLargest[2]) {
+    shiftAndUpdate(threeLargest, num, 2);
+  } else if (threeLargest[1] === null || num > threeLargest[1]) {
+    shiftAndUpdate(threeLargest, num, 1);
+  } else if (threeLargest[0] === null || num > threeLargest[0]) {
+    shiftAndUpdate(threeLargest, num, 0);
+  }
+}
+
+function shiftAndUpdate(array, num, idx) {
+  for (let i = 0; i <= idx; i++) {
+    if (i === idx) {
+      array[i] = num;
+    } else {
+      array[i] = array[i + 1];
+    }
+  }
 }
