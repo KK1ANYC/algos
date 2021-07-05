ae12891f5fc53ea8ad509ac1bf45bad0ba767ce9
Kevin Kim <kevin.kim9411@gmail.com>
2021-07-05 16:07:53 -0400

    divide and conquer findRotatedIndex

--- Algorithms/divideAndConquer.js
+++ Algorithms/divideAndConquer.js
@@ -76,3 +76,51 @@ function sortedFrequency(arr, target) {
 // console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)); // -1
 // console.log(sortedFrequency([1,1,1,1,1,1,1,1,1,1,1 2, 2, 2, 2, 3], 4)); // -1
 // console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3, 3,3,3,3,3,3,3,3,3,3], 4)); // -1
+
+//findRotatedIndex
+//unfinished
+function findRotatedIndex(arr, target){
+  // add whatever parameters you deem necessary - good luck!
+  let start = 0
+  let end = arr.length
+  let mid = Math.floor((start + end) / 2)
+  while (true) {
+    if (arr[mid] === target) return mid
+    if (arr[start] === target) return start
+    if (arr[end] === target) return end
+    if (end - start <= 1) break;
+    if (arr[mid] < target && arr[end] > target) {
+      start = mid
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] > target && arr[start] < target) {
+      end = mid
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] < target && arr[start] < target) {
+      end = mid
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] > target && arr[start] > target) {
+      start = mid
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] < target) {
+      start = mid
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] > target) {
+      end = mid
+      mid = Math.floor((start + end) / 2)
+    }
+  }
+  return -1
+}
+
+
+console.log(findRotatedIndex([3,4,1,2],4)) // 1
+console.log(findRotatedIndex([6,7,8,9,1,2,3,4],8)) // 2
+console.log(findRotatedIndex([6,7,8,9,1,2,3,4],3)) // 6
+console.log(findRotatedIndex([6,7,8,9,1,2,3,4], 12)) // -1
+console.log(findRotatedIndex([11,12,13,14,15,16,3,5,7,9],16)) // 5
+console.log(findRotatedIndex([11,12,13,14,15,16,17,18,7,9],7)) // 8
