d55c3430435a42791aa03ad38d294346269d9866
Kevin Kim <kevin.kim9411@gmail.com>
2021-07-15 17:57:03 -0400

    fixes format

--- leetcode/ransomNote.js
+++ leetcode/ransomNote.js
@@ -1,17 +1,17 @@
-var canConstruct = function(ransomNote, magazine) {
-	let memo = {};
-    if (magazine.length < ransomNote.length) return false
-    for (let i = 0; i < ransomNote.length; i++) {
-      if (memo[ransomNote[i]]) memo[ransomNote[i]]++
-      else memo[ransomNote[i]] = 1
+var canConstruct = function (ransomNote, magazine) {
+  let memo = {};
+  if (magazine.length < ransomNote.length) return false;
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (memo[ransomNote[i]]) memo[ransomNote[i]]++;
+    else memo[ransomNote[i]] = 1;
+  }
+  for (let j = 0; j < magazine.length; j++) {
+    if (memo[magazine[j]]) memo[magazine[j]]--;
+  }
+  for (let key in memo) {
+    if (memo[key] > 0) {
+      return false;
     }
-    for (let j = 0; j < magazine.length; j++) {
-      if (memo[magazine[j]]) memo[magazine[j]]--
-    }
-    for (let key in memo) {
-      if (memo[key] > 0) {
-	return false
-      }
-    }
-    return true
-  };
+  }
+  return true;
+};
