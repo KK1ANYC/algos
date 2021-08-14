f2b087651f6b4baf777a051d6523003e76e17ac6
Kevin Kim <kevin.kim9411@gmail.com>
2021-08-14 16:51:52 -0400

    two num sum solution 2

---
+++ algoExpert/Easy/twoNumSum.js
@@ -0,0 +1,39 @@
+let array = [3, 5, -4, 8, 11, 1, -1, 6];
+let targetSum = 10;
+
+
+
+console.log(twoNumberSum(array, targetSum));
+
+//solution 1 - Pointers
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left !== right) {
+    if (array[left] + array[right] === targetSum) {
+      return [array[left], array[right]];
+    }
+    if (array[left] + array[right] < targetSum) {
+      left++;
+    }
+    if (array[left] + array[right] > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
+
+//solution 2 - nested for loop
+function twoNumberSum(array, targtSum) {
+  for (let i = 0; i < array.length; i++) {
+    let firstNum = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      let secondNum = array[j];
+      if (firstNum + secondNum === targetSum) {
+        return [firstNum, secondNum];
+      }
+    }
+  }
+  return [];
+}
