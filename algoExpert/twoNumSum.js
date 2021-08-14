e27253f43f5ae136e3dedd6dfaf010aae0182cba
Kevin Kim <kevin.kim9411@gmail.com>
2021-08-14 16:46:46 -0400

    two num sum solution

--- algoExpert/twoNumSum.js
+++ algoExpert/twoNumSum.js
@@ -1,6 +1,23 @@
 let array = [3, 5, -4, 8, 11, 1, -1, 6];
 let targetSum = 10;
 
-function twoNumberSum() {
-	
+function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  let result = [];
+  while (left !== right) {
+    if (array[left] + array[right] === targetSum) {
+      return [array[left], array[right]]
+    }
+    if (array[left] + array[right] < targetSum) {
+      left++;
+    }
+    if (array[left] + array[right] > targetSum) {
+      right--;
+    }
+  }
+  return result
 }
+
+console.log(twoNumberSum(array, targetSum))
