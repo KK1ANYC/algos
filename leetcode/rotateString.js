5de8d22f5a9696780c16d49f5d3814a3c4db3f2a
Kevin Kim <kevin.kim9411@gmail.com>
2021-09-06 13:58:35 -0400

    rotate String example 2

--- leetcode/rotateString.js
+++ leetcode/rotateString.js
@@ -1,3 +1,4 @@
+//Example 1
 var rotateString = function (s, goal) {
   if (goal.length < s.length) return false;
   for (let i = 0; i < goal.length; i++) {
@@ -26,4 +27,30 @@ var rotateString = function (s, goal) {
     }
   }
   return true;
+};
+
+// Example 2
+var rotateString = function (str, goal) {
+  let rotatedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === goal[0] && str.slice(i) === goal.slice(0, str.length - i)) {
+      rotatedStr += str.slice(i);
+      rotatedStr += str.slice(0, i);
+      break;
+    }
+  }
+  if (rotatedStr === goal) return true;
+  else return false;
+};
+
+var rotateString = function (str, goal) {
+  let count = str.length;
+  while (count) {
+    let temp = str[0];
+    str = str.slice(1);
+    str += temp;
+    count--;
+    if (str === goal) return true;
+  }
+  return false;
 };
