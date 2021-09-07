fad40719455ce137235ef864ef40deb939dfaa7b
Kevin Kim <kevin.kim9411@gmail.com>
2021-09-06 20:15:43 -0400

    example 1 and 2

--- leetcode/backspaceStringCompare.js
+++ leetcode/backspaceStringCompare.js
@@ -1,3 +1,23 @@
+
+//example 1
+var backspaceCompare = function (s, t) {
+  let stack1 = [];
+  let stack2 = [];
+  let max = s.length >= t.length ? s.length : t.length;
+  let count = 0;
+  while (count <= max) {
+    if (s[count] !== "#") stack1.push(s[count]);
+    if (s[count] === "#") stack1.pop();
+    if (t[count] !== "#") stack2.push(t[count]);
+    if (t[count] === "#") stack2.pop();
+    count++;
+  }
+  return stack1.join("") === stack2.join("");
+};
+
+
+
+//example 2
 var backspaceCompare = function (s, t) {
   let stack1 = [];
   let stack2 = [];
