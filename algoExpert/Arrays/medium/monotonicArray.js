d324b45e6e47b6a5261eb7c9c5edba600212cbfc
Kevin Kim <kevin.kim9411@gmail.com>
2021-10-20 21:48:33 -0400

    is Monotonic solution 2

--- algoExpert/Arrays/medium/monotonicArray.js
+++ algoExpert/Arrays/medium/monotonicArray.js
@@ -7,3 +7,39 @@ function isMonotonic(array) {
   }
   return upTrend || downTrend;
 }
+
+
+function isMonotonic(array) {
+  // Write your code here.
+  let lastNum = array[0];
+  let direction = "";
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] >= array[i - 1]) {
+      direction = "increase";
+    }
+    if (array[i] <= array[i - 1]) {
+      direction = "decrease";
+    }
+  }
+
+  if (direction === "increase") {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] >= lastNum) {
+        lastNum = array[i];
+      } else {
+        return false;
+      }
+    }
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] <= lastNum) {
+        lastNum = array[i];
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
