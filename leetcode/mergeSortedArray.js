259ad21580c9e7171aef2aa609aef2f5f46661ac
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-02 17:21:04 -0400

    merge sorted array

--- leetcode/mergeSortedArray.js
+++ leetcode/mergeSortedArray.js
@@ -38,9 +38,13 @@ var merge = function (nums1, m, nums2, n) {
   for (let i = 0; i < m; i++) {
     nums1Copy.push(nums1[i]);
   }
+  let j = 0;
 
   for (let j = 0; j < m + n; j++) {
     if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+      // to ensure you dont step out of bounds
+      // if p2 is greater than or = to end we'll continue to replace our vals in nums1
+      // we also want to make sure that p1 is still less than m and that the number vals we are comparing are less
       nums1[j] = nums1Copy[p1++];
     } else {
       nums1[j] = nums2[p2++];
