e8d040fa2df2396b236cf5dcdbefdc2d3fc7bc08
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-02 17:09:31 -0400

    merge sorted array

--- leetcode/mergeSortedArray.js
+++ leetcode/mergeSortedArray.js
@@ -28,3 +28,23 @@ function merge(nums1, m, nums2, n) {
   }
   console.log(nums1);
 }
+
+
+var merge = function (nums1, m, nums2, n) {
+  let p1 = 0;
+  let p2 = 0;
+  let nums1Copy = [];
+
+  for (let i = 0; i < m; i++) {
+    nums1Copy.push(nums1[i]);
+  }
+
+  for (let j = 0; j < m + n; j++) {
+    if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+      nums1[j] = nums1Copy[p1++];
+    } else {
+      nums1[j] = nums2[p2++];
+    }
+  }
+  return nums1;
+};
