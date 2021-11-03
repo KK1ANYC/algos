8733cc74d8ff98515a8003817f23f4f4d7007037
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-03 18:37:33 -0400

    first unique char in string solution

---
+++ leetcode/firstUniqueChar.js
@@ -0,0 +1,21 @@
+var firstUniqChar = function (s) {
+  const uniqSet = new Set(); //
+  let firstKey = undefined;
+  for (const char of s) {
+    if (!uniqSet[char]) uniqSet[char] = 1;
+    else uniqSet[char]++;
+  }
+
+  for (const key in uniqSet) {
+    if (uniqSet[key] === 1) {
+      firstKey = key;
+      break;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === firstKey) return i;
+  }
+
+  return -1;
+};
