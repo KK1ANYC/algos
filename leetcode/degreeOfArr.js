d8fb5c33d45c8d9c508649e206df4817c85b424c
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-03 18:16:03 -0400

    first unique char in string

--- leetcode/degreeOfArr.js
+++ leetcode/degreeOfArr.js
@@ -25,3 +25,51 @@ function findShortestSubArray(nums) {
 }
 
 
+//WIP incomplete
+var findShortestSubArray = function(nums) {
+    let memo = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (!memo[nums[i]]) memo[nums[i]] = 1
+        else memo[nums[i]]++
+    }
+    let max = -Infinity
+    let maxNum = 0
+    let maxNumArr = []
+    for (let key in memo) {
+      // console.log("memo[key]", memo[key])
+        if (memo[key] > max) {
+            max = memo[key]
+            maxNum = key
+        }
+        else if (memo[key] === max) {
+          maxNumArr.push(key, maxNum)
+        }
+    }
+    // console.log("maxNum", maxNum)
+    // console.log("maxNumArr", maxNumArr)
+    let minIdx = -Infinity
+    let maxIdx = Infinity
+    let count = 0
+    let hash = {}
+    while (count <= maxNumArr.length) {
+      let firstIdx = 0
+      console.log("maxNumArr count", maxNumArr[count])
+      for (let i = 0; i < nums.length; i++) {
+          if (nums[i] === maxNumArr[count]) {
+              firstIdx = i
+          }
+      }
+      for (let i = nums.length; i > 0; i--) {
+          if (nums[i] === maxNumArr[count]) {
+              hash[maxNumArr[count]] = [firstIdx, i]
+          }
+      }
+      count++
+    }
+    console.log("hash", hash)
+
+    let subArr = nums.slice(minIdx, maxIdx)
+    return subArr.length
+};
+
+console.log(findShortestSubArray([1,2,2,3,1])) //2
