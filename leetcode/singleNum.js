e650d0e117323287fb59f63f58624072eb9891cc
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-05 22:17:22 -0400

    single number solution

---
+++ leetcode/singleNum.js
@@ -0,0 +1,11 @@
+var singleNumber = function (nums) {
+  let memo = {};
+  for (let i = 0; i < nums.length; i++) {
+    memo[nums[i]] ? memo[nums[i]]++ : (memo[nums[i]] = 1);
+  }
+  for (let key in memo) {
+    if (memo[key] === 1) {
+      return Number(key);
+    }
+  }
+};
