96f3725a233315dbe9526155ad77a3cf3e6de501
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-06 14:37:40 -0400

    palindrome number comment

--- leetcode/palindromeNumber.js
+++ leetcode/palindromeNumber.js
@@ -1,2 +1,17 @@
 //O(n) linear time
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  let str = String(x);
+  let p1 = 0;
+  let p2 = str.length - 1;
+  while (p1 < p2) {
+    if (str[p1] === str[p2]) {
+      p1++;
+      p2--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
 
