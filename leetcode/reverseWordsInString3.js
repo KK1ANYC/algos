1d9a593029f0cfe317fdf5484744eb8a29e1a795
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-06 14:29:11 -0400

    reverse words in string solution

--- leetcode/reverseWordsInString3.js
+++ leetcode/reverseWordsInString3.js
@@ -1,2 +1,16 @@
 //O(n^2) nested for loop
 
+var reverseWords = function (s) {
+  let sentence = s.split(" ");
+  let result = [];
+  let words = "";
+  for (let i = 0; i < sentence.length; i++) {
+    let word = sentence[i];
+    for (let k = word.length - 1; k >= 0; k--) {
+      words += word[k];
+    }
+    result.push(words);
+    words = "";
+  }
+  return result.join(" ");
+};
