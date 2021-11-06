25b2728408d2376fa1b10152ed5f4462580cebca
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-06 14:14:44 -0400

    two sum new solution

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -13,6 +13,19 @@ function twoSum(nums, target) {
   return [];
 }
 
-
+//O(n) space and time, linear
+var twoSum = function (nums, target) {
+  let memo = {};
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (diff in memo) {
+      result = [memo[diff], i];
+    } else {
+      memo[nums[i]] = i;
+    }
+  }
+  return result;
+};
 
 console.log(twoSum([3, 2, 4], 6));
