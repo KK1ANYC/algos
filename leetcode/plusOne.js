203e69eece791db0bcefe8219834c746c4550f9c
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-07 19:16:14 -0500

    plus one solution two

--- leetcode/plusOne.js
+++ leetcode/plusOne.js
@@ -9,6 +9,19 @@ function plusOne(digits) {
   return digits;
 }
 
+let plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i]++;
+    if (digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+};
+
 
 console.log(plusOne([9,9])) //[1,0,0]
 console.log(plusOne([1,2,3])) //[1,2,4]
