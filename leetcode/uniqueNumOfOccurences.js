1cf415ce6ab35e2591c583003aeef921d4fe7512
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-07 19:51:46 -0500

    unique number of occurrences console log

--- leetcode/uniqueNumOfOccurences.js
+++ leetcode/uniqueNumOfOccurences.js
@@ -12,3 +12,9 @@ var uniqueOccurrences = function (arr) {
   }
   return true;
 };
+
+
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3])); //true
+console.log(uniqueOccurrences([1, 2])); //false
+console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0])); //true
+
