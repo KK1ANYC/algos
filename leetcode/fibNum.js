8e0e2be70bb01ed8f1d72d71e4f17aedb49649f2
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-10 19:13:00 -0500

    fib num solution

--- leetcode/fibNum.js
+++ leetcode/fibNum.js
@@ -1,3 +1,13 @@
 /*
 caching the result of the fib
 */
+
+var fib = function (n) {
+  let result = 0;
+  let hash = { 0: 0, 1: 1 };
+  for (let i = 2; i <= n; i++) {
+    hash[i] = hash[i - 2] + hash[i - 1];
+  }
+  return hash[n];
+};
+
