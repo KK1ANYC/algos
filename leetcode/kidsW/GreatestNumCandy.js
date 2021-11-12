a5eb5ef7e46baaf12769a905695fdbd76239553b
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-12 15:46:44 -0500

    kids with the greatest number of candies explanation

--- leetcode/kidsW/GreatestNumCandy.js
+++ leetcode/kidsW/GreatestNumCandy.js
@@ -21,3 +21,17 @@ var kidsWithCandies = function (candies, extraCandies) {
   return result;
 };
 
+
+/*
+return boolean array
+true if candies[i] + extra is greater than the kid with most candies
+false otherwise
+multiple kids can be true
+
+find the greatest candy by looping through candies
+
+then loop through it again to see if each kid with x candies + extra candies has greater than the most candy
+push true if they do and false if they dont into a result array
+
+return result
+*/
