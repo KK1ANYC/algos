f731e3dd6a2397b7ada10a744abedf49e2027909
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-12 16:58:04 -0500

    merge sorted array solution clean up

--- leetcode/mergeSortedArray.js
+++ leetcode/mergeSortedArray.js
@@ -1,34 +1,3 @@
-function merge(nums1, m, nums2, n) {
-  let p1 = 0;
-  let p2 = 0;
-  while (p1 < m || p2 < n) {
-    if (p1 >= m && p2 < n) {
-      nums1.pop();
-      nums1.splice(p1, 0, nums2[p2]);
-      p2++;
-      p1++;
-    } else if (nums2[p2] !== undefined && nums1[p1] > nums2[p2]) {
-      nums1.pop();
-      nums1.splice(p1, 0, nums2[p2]);
-      p2++;
-      m++;
-    } else if (
-      nums2[p2] !== undefined &&
-      nums1[p1] <= nums2[p2] &&
-      nums1[p1 + 1] > nums2[p2]
-    ) {
-      nums1.pop();
-      nums1.splice(p1 + 1, 0, nums2[p2]);
-      p2++;
-      p1++;
-      m++;
-    } else {
-      p1++;
-    }
-  }
-  console.log(nums1);
-}
-
 
 var merge = function (nums1, m, nums2, n) {
   let p1 = 0;
@@ -52,6 +21,7 @@ var merge = function (nums1, m, nums2, n) {
   }
   return nums1;
 };
+
 
 var merge = function (nums1, m, nums2, n) {
   let write = 0;
@@ -77,37 +47,3 @@ var merge = function (nums1, m, nums2, n) {
 };
 
 
-
-//doesn't work but have to fix
-var merge = function (nums1, m, nums2, n) {
-    let p1 = 0
-    let p2 = 0
-    while (p2 < n) {
-      nums2.sort((a,b) => a-b)
-      if (nums1[p1] > nums2[p2]) {
-        let temp = nums2[p2]
-        nums2[p2] = nums1[p1]
-        nums1[p1] = temp
-        p1++
-        console.log("hit nums1 > nums2")
-      }
-      if (nums1[p1] === 0) {
-        let temp = nums2[p2]
-        nums2[p2] = nums1[p1]
-        nums1[p1] = temp
-        p2++
-        console.log("hit nums1 == 0")
-      }
-      if (nums1[p1] < nums2[p2]) {
-        p1++
-        console.log("hit nums1 < nums2")
-      }
-      if (nums1[p1] === nums2[p2]) {
-        p1++
-        console.log("hit equal")
-      }
-      console.log("nums1", nums1, "p1", p1)
-      console.log("nums2", nums2, "p2", p2)
-    }
-    return nums1
-};
