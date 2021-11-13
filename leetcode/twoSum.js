4701249370ca548a2ae71750798302ae5076c300
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-13 18:18:34 -0500

    two sum variation solution

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -34,4 +34,18 @@ console.log(twoSum([3, 2, 4], 6));
 O(n) time, linear
 return the (index + 1) of the two numbers that add up to the target
 */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
 
+  while (left < right) {
+    let total = numbers[left] + numbers[right];
+    if (total === target) {
+      return [left + 1, right + 1];
+    } else if (total < target) {
+      left++;
+    } else if (total > target) {
+      right--;
+    }
+  }
+};
