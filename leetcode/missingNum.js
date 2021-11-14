6ab4e7d2cd1a85098fb7fe19ab50eac91e17ebe4
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-13 19:20:47 -0500

    missing number solution

---
+++ leetcode/missingNum.js
@@ -0,0 +1,17 @@
+var missingNumber = function (nums) {
+  let hash = {};
+  for (let num of nums) {
+    if (!hash[num]) hash[num] = 1;
+    else hash[num]++;
+  }
+
+  let totalNums = nums.length;
+
+  while (totalNums >= 0) {
+    if (hash[totalNums]) {
+      totalNums--;
+    } else {
+      return totalNums;
+    }
+  }
+};
