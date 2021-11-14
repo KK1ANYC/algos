e432035c99b4cfe3a8794785bedc2d3201ab3d51
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-13 19:31:58 -0500

    largest unique number solution

--- leetcode/largestUniqueNum.js
+++ leetcode/largestUniqueNum.js
@@ -1,2 +1,21 @@
 //O(n) time and space
 
+var largestUniqueNumber = function (nums) {
+  let hash = {};
+  for (let num of nums) {
+    if (!hash[num]) hash[num] = 1;
+    else hash[num]++;
+  }
+
+  let maxNum = -1;
+
+  for (let key in hash) {
+    if (hash[key] === 1 && +key > maxNum) {
+      maxNum = +key;
+    }
+  }
+
+  return maxNum;
+};
+
+
