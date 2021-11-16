68e0c71ae4ca682da8727cf367140e4cb6e99c5e
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-16 18:22:25 -0500

    string matching in an array explanation

--- leetcode/stringMatchInAnArr.js
+++ leetcode/stringMatchInAnArr.js
@@ -24,3 +24,25 @@ var stringMatching = function (words) {
   }
   return ansArr;
 };
+
+
+
+/*
+O(n^2) Time O(n) Space
+
+I solved this using a nested for loop,
+
+I essentially checked every single combination possible.
+ i        j
+['mass', 'as, 'hero', 'superhero']
+
+this is how my loop would start.
+
+Within each inner for loop I would check to see if words.includes otherWords
+ and if it did I had to ensure that it wasn't the same exact word, if it was we just continue on with our loop else
+I'm going to add it to our set.
+
+I also had to ensure that my otherWord never went out of bounds either, and vice versa.
+
+Once I had all the values within my set I simply looped through my set and created an array out of it'
+*/
