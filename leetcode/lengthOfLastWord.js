23f51292fe8d3b173e4ed6930b4b814a740d2404
KK1ANYC <kevin.kim9411@gmail.com>
2021-11-24 19:09:26 -0500

    length of last word solution

--- leetcode/lengthOfLastWord.js
+++ leetcode/lengthOfLastWord.js
@@ -1,1 +1,14 @@
 //O(n)
+var lengthOfLastWord = function (s) {
+  let word = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (word.length >= 1 && s[i] === " ") {
+      break;
+    }
+    if (s[i] !== " ") {
+      word += s[i];
+    }
+  }
+  return word.length;
+};
+
