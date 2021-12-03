2937b16ddbe07efa0dc59363b83add364ec626e1
KK1ANYC <kevin.kim9411@gmail.com>
2021-12-03 16:41:19 -0500

    amzn assessment

--- company interviews/amzn.js
+++ company interviews/amzn.js
@@ -1,3 +1,4 @@
+//Altus
 var vowelConsonantsSequence = function (password) {
   const vowels = new Set("aeiou");
   let subStringHasVowel = 0;
@@ -20,6 +21,28 @@ var vowelConsonantsSequence = function (password) {
     ? amountOfSequence + 1
     : amountOfSequence;
 };
+
+//Kevin
+function findPasswordStrength(password) {
+  const vowels = new Set("aeiou");
+  let result = 0;
+  let hasVowel = false;
+  let hasconsonant = false;
+  for (let i = 0; i < password.length; i++) {
+    if (hasVowel && hasconsonant) {
+      console.log("hit both", password[i]);
+      hasVowel = false;
+      hasconsonant = false;
+      result++;
+    }
+    if (vowels.has(password[i])) {
+      hasVowel = true;
+    } else if (!vowels.has(password[i])) {
+      hasconsonant = true;
+    }
+  }
+  return hasVowel && hasconsonant ? result + 1 : result;
+}
 
 console.log(vowelConsonantsSequence("hackerrank")); //3
 console.log(vowelConsonantsSequence("thisisbeautiful")); //6
