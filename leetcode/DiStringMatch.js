278c2c4dac58fc578659b5e3ec4ec2bcce926b2f
KK1ANYC <kevin.kim9411@gmail.com>
2021-12-09 16:04:11 -0500

    DI String Match explanation

--- leetcode/DiStringMatch.js
+++ leetcode/DiStringMatch.js
@@ -15,3 +15,15 @@ var diStringMatch = function (s) {
   }
   return perm;
 };
+
+/*
+create a variable of low and high that increments and decrements based on the string at index i
+Low and High will also keep track of numbers that are added to the array
+variable i will be a pointer
+have a perm array as empty for the result
+while loop that will run until low is greater than high
+if s[i] is I, push low to the array, and increment low
+else push high to the array, and decrement high
+incremenet i regardless
+return the array
+*/
