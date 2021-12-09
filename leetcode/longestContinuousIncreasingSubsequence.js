60eae6a63756b93c7f7bac1ff75ff83351984ec9
KK1ANYC <kevin.kim9411@gmail.com>
2021-12-09 16:47:57 -0500

    longest continuous increasing subsequence solution

--- leetcode/longestContinuousIncreasingSubsequence.js
+++ leetcode/longestContinuousIncreasingSubsequence.js
@@ -1,1 +1,21 @@
 //Sliding Window
+var findLengthOfLCIS = function (nums) {
+  let result = -Infinity;
+  let count = 1;
+  let start = 0;
+  let end = 0;
+  while (end < nums.length) {
+    if (nums[end] < nums[end + 1]) {
+      count++;
+      end++;
+    } else {
+      result = Math.max(result, count);
+      end = end + 1;
+      start = end;
+      count = 1;
+    }
+  }
+  return result;
+};
+
+
