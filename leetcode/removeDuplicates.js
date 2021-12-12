74b3739f23f552940f96eb5c711a08906b2e5dd6
KK1ANYC <kevin.kim9411@gmail.com>
2021-12-12 18:23:00 -0500

    remove duplicates from sorted array solution

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -1,19 +1,21 @@
-
-
-function removeDuplicates(nums) {
-  let count = 0
-    while(count < nums.length - 1) {
-      // console.log("running")
-      // console.log(nums.length)
-      // console.log(count)
-      if (nums[count] === nums[count+1]) {
-        nums.splice(count, 1)
-        count--
-      }
-      count++
+var removeDuplicates = function (nums) {
+  let fast = 0;
+  let slow = 0;
+  while (fast < nums.length) {
+    if (nums[slow] === nums[fast]) {
+      fast++;
+    } else if (nums[slow] !== nums[fast]) {
+      slow++;
+      nums[slow] = nums[fast];
+    } else {
+      fast++;
     }
-    return nums
-}
+  }
 
-console.log(removeDuplicates([1,1,2])) // [1,2,_]
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])) // [0,1,2,3,4,_,_,_,_,_]
+  for (let i = nums.length - 1; i > slow; i--) {
+    nums.pop();
+  }
+};
+
+console.log(removeDuplicates([1, 1, 2])); // [1,2,_]
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // [0,1,2,3,4,_,_,_,_,_]
