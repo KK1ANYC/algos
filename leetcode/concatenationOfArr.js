245452e4f15f2712ac16e519d1bd6ff98bd7e747
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-03 12:34:36 -0500

    concatenation of array solution

---
+++ leetcode/concatenationOfArr.js
@@ -0,0 +1,12 @@
+var getConcatenation = function (nums) {
+  let result = new Array();
+  for (let i = 0; i < nums.length; i++) {
+    result.push(nums[i]);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    result.push(nums[i]);
+  }
+
+  return result;
+};
