96e35ce278dde99e7c9464691985bfda7cb1d877
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-03 02:25:59 -0500

    max product of two elements in an array solution

---
+++ leetcode/maxProductTwoElArr.js
@@ -0,0 +1,37 @@
+var maxProduct = function (nums) {
+  const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    if (left < right) {
+      let pivotIdx = pivot(arr, left, right);
+      quickSort(arr, left, pivotIdx - 1);
+      quickSort(arr, pivotIdx + 1, right);
+    }
+    return arr;
+  };
+
+  const pivot = (arr, start = 0, end = arr.length - 1) => {
+    const swap = (arr, i, j) => {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    };
+
+    let pivot = arr[start];
+    let swapIndex = start;
+
+    for (let i = start + 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        swapIndex++;
+        swap(arr, swapIndex, i);
+      }
+    }
+    swap(arr, start, swapIndex);
+    return swapIndex;
+  };
+
+  nums = quickSort(nums);
+
+  let num1 = nums[nums.length - 1];
+  let num2 = nums[nums.length - 2];
+
+  return (num1 - 1) * (num2 - 1);
+};
