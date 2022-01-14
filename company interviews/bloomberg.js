bb6a1e51cd7fdb812610b956188ce433c4706b53
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-13 20:21:44 -0500

    isAnagram

--- company interviews/bloomberg.js
+++ company interviews/bloomberg.js
@@ -1,5 +1,49 @@
 //Easy
+
 https://leetcode.com/problems/reverse-linked-list/
 
 https://leetcode.com/problems/binary-tree-paths/
+
+https://leetcode.com/problems/first-unique-character-in-a-string/
+// O(n) time complexity
+var firstUniqChar = function (s) {
+  let hash = {};
+  let result = -1;
+
+  for (char of s) {
+    if (!hash[char]) hash[char] = 1;
+    else hash[char]++;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (hash[s[i]] == 1) {
+      result = i;
+      break;
+    }
+  }
+  return result;
+};
+
+https://leetcode.com/problems/valid-anagram/
+//O(n) time complexity
+var isAnagram = function(s, t) {
+    let hash = {}
+    let result = true
+
+    if (s.length !== t.length) return false
+
+    for (let i = 0; i < s.length; i++) {
+        if (!hash[s[i]]) hash[s[i]] = 1
+        else hash[s[i]]++
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (hash[t[i]]) hash[t[i]]--
+        else {
+            return false
+        }
+    }
+
+    return result
+};
 
