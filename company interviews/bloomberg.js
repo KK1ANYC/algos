034cfac9d47ebab292a56fa7a8ed31812a8715b6
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-16 21:54:26 -0500

    add two numbers 2

--- company interviews/bloomberg.js
+++ company interviews/bloomberg.js
@@ -1,40 +1,39 @@
 //Easy
 
-https://leetcode.com/problems/reverse-linked-list/
+//https://leetcode.com/problems/reverse-linked-list/
 //O(n) time complexity
 var reverseList = function (head) {
-    let pre = null
-    while (head) {
-        const next = head.next
-        head.next = pre
-        pre = head
-        head = next
-    }
-    return pre
+  let pre = null;
+  while (head) {
+    const next = head.next;
+    head.next = pre;
+    pre = head;
+    head = next;
+  }
+  return pre;
 };
 
+//https://leetcode.com/problems/binary-tree-paths/
+var binaryTreePaths = function (root) {
+  let result = new Array();
+  dfs(root, "");
 
-https://leetcode.com/problems/binary-tree-paths/
-var binaryTreePaths = function(root) {
-    let result = new Array()
-    dfs(root, "")
-
-    function dfs (root, currentPath) {
-        if (root === null) {
-            return
-        }
-        if (root.left == null && root.right == null) {
-            currentPath += root.val
-            result.push(currentPath)
-            return
-        }
-        dfs(root.left, currentPath + root.val + `->`)
-        dfs(root.right, currentPath + root.val + `->`)
+  function dfs(root, currentPath) {
+    if (root === null) {
+      return;
     }
-    return result
+    if (root.left == null && root.right == null) {
+      currentPath += root.val;
+      result.push(currentPath);
+      return;
+    }
+    dfs(root.left, currentPath + root.val + `->`);
+    dfs(root.right, currentPath + root.val + `->`);
+  }
+  return result;
 };
 
-https://leetcode.com/problems/first-unique-character-in-a-string/
+//https://leetcode.com/problems/first-unique-character-in-a-string/
 // O(n) time complexity
 var firstUniqChar = function (s) {
   let hash = {};
@@ -54,147 +53,138 @@ var firstUniqChar = function (s) {
   return result;
 };
 
-https://leetcode.com/problems/valid-anagram/
+//https://leetcode.com/problems/valid-anagram/
 //O(n) time complexity
-var isAnagram = function(s, t) {
-    let hash = {}
-    let result = true
+var isAnagram = function (s, t) {
+  let hash = {};
+  let result = true;
 
-    if (s.length !== t.length) return false
+  if (s.length !== t.length) return false;
 
-    for (let i = 0; i < s.length; i++) {
-        if (!hash[s[i]]) hash[s[i]] = 1
-        else hash[s[i]]++
+  for (let i = 0; i < s.length; i++) {
+    if (!hash[s[i]]) hash[s[i]] = 1;
+    else hash[s[i]]++;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (hash[t[i]]) hash[t[i]]--;
+    else {
+      return false;
     }
+  }
 
-    for (let i = 0; i < t.length; i++) {
-        if (hash[t[i]]) hash[t[i]]--
-        else {
-            return false
-        }
-    }
-
-    return result
+  return result;
 };
 
-https://leetcode.com/problems/palindrome-number/
+//https://leetcode.com/problems/palindrome-number/
 // O(n) time complexity
 function isPalindrome(x) {
-    if (x < 0) return false
+  if (x < 0) return false;
 
-    x = x.toString()
+  x = x.toString();
 
-    let left = 0
-    let right = x.length - 1
+  let left = 0;
+  let right = x.length - 1;
 
-    while (left <= right) {
-        if (x[left] !== x[right]) return false
-        left++
-        right--
-    }
+  while (left <= right) {
+    if (x[left] !== x[right]) return false;
+    left++;
+    right--;
+  }
 
-    return true
+  return true;
 }
 
-https://leetcode.com/problems/intersection-of-two-linked-lists/
+//https://leetcode.com/problems/intersection-of-two-linked-lists/
 // O(n) time complexity
 var getIntersectionNode = function (headA, headB) {
-    let hA = headA;
-    let hB = headB;
-    while (hA !== hB) {
-        hA = hA === null ? headB : hA.next;
-        hB = hB === null ? headA : hB.next;
-    };
-    return hA;
+  let hA = headA;
+  let hB = headB;
+  while (hA !== hB) {
+    hA = hA === null ? headB : hA.next;
+    hB = hB === null ? headA : hB.next;
+  }
+  return hA;
 };
 
-https://leetcode.com/problems/two-sum/
+//https://leetcode.com/problems/two-sum/
 //O(n) time complexity
 var twoSum = function (nums, target) {
-    let hash = {}
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        if (hash[diff] >= 0) {
-            return [hash[diff], i]
-        } else if (!hash[nums[i]]) {
-            hash[nums[i]] = i
-        }
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (hash[diff] >= 0) {
+      return [hash[diff], i];
+    } else if (!hash[nums[i]]) {
+      hash[nums[i]] = i;
     }
+  }
 };
 
-https://leetcode.com/problems/min-stack/
-var MinStack = function() {
-    this.stack = []
+//https://leetcode.com/problems/min-stack/
+var MinStack = function () {
+  this.stack = [];
 };
 
-/**
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function(val) {
-    this.stack.push(val)
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
 };
 
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-    if (this.stack.length > 0) {
-        this.stack.pop()
+MinStack.prototype.pop = function () {
+  if (this.stack.length > 0) {
+    this.stack.pop();
+  }
+};
+
+MinStack.prototype.top = function () {
+  if (this.stack.length > 0) {
+    return this.stack[this.stack.length - 1];
+  }
+};
+
+MinStack.prototype.getMin = function () {
+  let min = Infinity;
+  if (this.stack.length > 0) {
+    for (let i = 0; i < this.stack.length; i++) {
+      if (this.stack[i] < min) {
+        min = this.stack[i];
+      }
     }
+  }
+  return min;
 };
 
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-    if (this.stack.length > 0) {
-        return this.stack[this.stack.length - 1]
-    }
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-    let min = Infinity
-    if (this.stack.length > 0) {
-        for (let i = 0; i < this.stack.length; i++) {
-            if (this.stack[i] < min) {
-                min = this.stack[i]
-            }
-        }
-    }
-    return min
-};
-
-https://leetcode.com/problems/valid-parentheses/
+//https://leetcode.com/problems/valid-parentheses/
 //O(n) time complexity
-var isValid = function(s) {
-    let hash = {
-        ")": "(",
-        "}": "{",
-        "]": "[",
-    }
+var isValid = function (s) {
+  let hash = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
 
-    let arr = new Array()
+  let arr = new Array();
 
-    for (let i = 0; i < s.length; i++) {
-        let lastEl = arr[arr.length - 1]
-        if (lastEl === hash[s[i]] && lastEl !== undefined && hash[s[i]] !== undefined){
-            arr.pop()
-        } else {
-            arr.push(s[i])
-        }
-    }
-    if (arr.length == 0) {
-        return true
+  for (let i = 0; i < s.length; i++) {
+    let lastEl = arr[arr.length - 1];
+    if (
+      lastEl === hash[s[i]] &&
+      lastEl !== undefined &&
+      hash[s[i]] !== undefined
+    ) {
+      arr.pop();
     } else {
-        return false
+      arr.push(s[i]);
     }
+  }
+  if (arr.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-https://leetcode.com/problems/merge-two-sorted-lists/
+//https://leetcode.com/problems/merge-two-sorted-lists/
 //O(2 n) time complexity
 var mergeTwoLists = function (l1, l2) {
   let list = new ListNode();
@@ -221,4 +211,73 @@ var mergeTwoLists = function (l1, l2) {
 
   // return .next because this first element in the linkedlist is empty
   return head.next;
+};
+
+//Medium
+//https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/
+
+//https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+
+//https://leetcode.com/problems/sort-characters-by-frequency/
+//O(n^2) time complexity
+var frequencySort = function (s) {
+  let hash = {};
+  let result = "";
+
+  for (let char of s) {
+    if (!hash[char]) hash[char] = 1;
+    else hash[char]++;
+  }
+
+  while (result.length !== s.length) {
+    let max = 0;
+    let char = "";
+
+    for (let key in hash) {
+      if (hash[key] > max) {
+        max = hash[key];
+        char = key;
+      }
+    }
+
+    for (let i = 0; i < max; i++) {
+      result += char;
+    }
+
+    hash[char] = 0;
+  }
+
+  return result;
+};
+
+//https://leetcode.com/problems/add-two-numbers-ii/
+//O(n) time complexity
+var addTwoNumbers = function (l1, l2) {
+  let stack1 = [];
+  let stack2 = [];
+
+  while (l1 !== null || l2 !== null) {
+    if (l1) {
+      stack1.push(l1.val);
+      l1 = l1.next;
+    }
+    if (l2) {
+      stack2.push(l2.val);
+      l2 = l2.next;
+    }
+  }
+
+  let l3 = new ListNode(0);
+
+  while (stack1.length || stack2.length) {
+    let sum = 0;
+    if (stack1.length) sum += stack1.pop();
+    if (stack2.length) sum += stack2.pop();
+    sum += l3.val;
+    l3.val = sum % 10;
+    let head = new ListNode(Math.floor(sum / 10));
+    head.next = l3;
+    l3 = head;
+  }
+  return l3.val === 0 ? l3.next : l3;
 };
