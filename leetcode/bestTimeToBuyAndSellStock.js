797263a711da71cf5522cf10be68f1b6a01f89f2
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-18 15:28:59 -0500

    besdt time to buy and sell stock solution

---
+++ leetcode/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,16 @@
+var maxProfit = function (prices) {
+  let profit = 0;
+  let bp = 0;
+  let sp = 1;
+
+  while (sp < prices.length) {
+    let diff = prices[sp] - prices[bp];
+    if (prices[sp] < prices[bp]) {
+      bp = sp;
+    }
+    if (profit < diff) profit = diff;
+    sp++;
+  }
+
+  return profit;
+};
