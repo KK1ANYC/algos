c208cbc77c025ee6ef776ed75ffbe323d1b6f705
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-19 09:18:13 -0500

    search in rotated sorted array solution

---
+++ leetcode/searchInRotatedSortedArr.js
@@ -0,0 +1,31 @@
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    }
+    if (nums[left] == target) {
+      return left;
+    }
+    if (nums[right] == target) {
+      return right;
+    }
+    if (nums[left] <= nums[mid]) {
+      if (target > nums[mid] || target < nums[left]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    } else {
+      if (target < nums[mid] || target > nums[right]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return -1;
+};
