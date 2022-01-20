85268d27130f265012b170f87d3ed420aa96ce12
KK1ANYC <kevin.kim9411@gmail.com>
2022-01-19 19:29:04 -0500

    container with most water solution

---
+++ leetcode/containerWithMostWater.js
@@ -0,0 +1,15 @@
+var maxArea = function (height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    let area = Math.min(height[right], height[left]) * (right - left);
+    if (area > max) max = area;
+    if (height[right] <= height[left]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return max;
+};
